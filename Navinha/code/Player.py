e078ad1c0e14bf0874e6777b7d7d06437640c3ff
ViniciusPedrassi <marcuspedrassi@gmail.com>
2024-04-12 19:01:27 -0300

    criação de players, enemys e movimentação

--- Navinha/code/Player.py
+++ Navinha/code/Player.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
+import pygame
 
-from Entity import Entity
+from code.Const import ENTITY_SPEED, WIN_HEIGHT, WIN_WIDTH, PLAYER_KEY_UP, PLAYER_KEY_DOWN, PLAYER_KEY_LEFT, \
+    PLAYER_KEY_RIGHT
+from code.Entity import Entity
 
 class Player(Entity):
-    def __init__(self):
-        pass
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
 
     def move(self, ):
-        pass
+        pressed_key = pygame.key.get_pressed()
+        if pressed_key[PLAYER_KEY_UP[self.name]] and self.rect.top > 0: #se a tecla seta cima ou 'w' foi pressionada
+            self.rect.centery -= ENTITY_SPEED[self.name]
+
+        if pressed_key[PLAYER_KEY_DOWN[self.name]] and self.rect.bottom < WIN_HEIGHT: #se a tecla seta baixo ou 's' foi pressionada
+            self.rect.centery += ENTITY_SPEED[self.name]
+
+        if pressed_key[PLAYER_KEY_LEFT[self.name]] and self.rect.left > 0: #se a tecla seta esquerda ou 'a' foi pressionada
+            self.rect.centerx -= ENTITY_SPEED[self.name]
+
+        if pressed_key[PLAYER_KEY_RIGHT[self.name]] and self.rect.right < WIN_WIDTH: #se a tecla seta direita ou 'd' foi pressionada
+            self.rect.centerx += ENTITY_SPEED[self.name]
+
+
 
