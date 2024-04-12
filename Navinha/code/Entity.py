00314f57f3de33317823569d72c099fe35b2c1cf
ViniciusPedrassi <marcuspedrassi@gmail.com>
2024-04-12 19:01:27 -0300

    criação de players, enemys e movimentação

--- Navinha/code/Entity.py
+++ Navinha/code/Entity.py
@@ -4,6 +4,8 @@ from abc import ABC
 
 import pygame.image
 
+from code.Const import ENTITY_HEALTH
+
 
 def abtrasctmethod(args):
     pass
@@ -12,9 +14,10 @@ def abtrasctmethod(args):
 class Entity(ABC):
     def __init__(self, name: str, position: tuple):
         self.name = name
-        self.surf = pygame.image.load('./asset/' + name + '.png')
+        self.surf = pygame.image.load('./asset/' + self.name + '.png').convert_alpha()
         self.rect = self.surf.get_rect(left= position[0], top=position[1])
         self.speed = 0
+        self.health = ENTITY_HEALTH[self.name]
 
 
     @abtrasctmethod
--- Navinha/code/Menu.py
+++ Navinha/code/Menu.py
@@ -55,6 +55,6 @@ class Menu:
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
-        text_surf: Surface = text_font.render(text, True, text_color)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
         text_rect: Rect = text_surf.get_rect(center=text_center_pos)
         self.window.blit(source=text_surf, dest=text_rect)
